d75e48a6fdd4da10b8dc2747b7f8e326ece5c4e4
jiyasom <36759455+jiyasom@users.noreply.github.com>
2023-02-02 16:24:40 +0400

    completed working with events

---
+++ src/event/entities/event.entity.ts
@@ -0,0 +1,18 @@
+import { Entity, Column, PrimaryGeneratedColumn, Index } from 'typeorm';
+
+@Index(['type', 'name'])
+@Entity()
+export class Event {
+    @PrimaryGeneratedColumn()
+    id: number;
+
+    @Column()
+    type: string;
+
+    @Column()
+    name: string;
+
+    @Column('json')
+    payload: Record<string, any>;
+
+}
